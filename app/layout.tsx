cf472a53b4f5986ca1a3b4b93ab9593b91ecadf6
trainymonked <47102288+trainymonked@users.noreply.github.com>
2023-12-24 01:37:10 +0300

    Sidebar Layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import { Figtree } from 'next/font/google'
 import './globals.css'
+import Sidebar from '@/components/Sidebar'
 
 const font = Figtree({ subsets: ['latin'] })
 
@@ -12,7 +13,9 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
         <html lang='en'>
-            <body className={font.className}>{children}</body>
+            <body className={font.className}>
+                <Sidebar>{children}</Sidebar>
+            </body>
         </html>
     )
 }
