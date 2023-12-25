842e65260b62aca967a748fce252de040e5744d8
trainymonked <47102288+trainymonked@users.noreply.github.com>
2023-12-25 10:08:50 +0300

    Auth & Modals

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,8 @@ import './globals.css'
 import Sidebar from '@/components/Sidebar'
 import SupabaseProvider from '@/providers/SupabaseProvider'
 import UserProvider from '@/providers/UserProvider'
+import ModalProvider from '@/providers/ModalProvider'
+import ToasterProvider from '@/providers/ToasterProvider'
 
 const font = Figtree({ subsets: ['latin'] })
 
@@ -17,8 +19,10 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     return (
         <html lang='en'>
             <body className={font.className}>
+                <ToasterProvider />
                 <SupabaseProvider>
                     <UserProvider>
+                        <ModalProvider />
                         <Sidebar>{children}</Sidebar>
                     </UserProvider>
                 </SupabaseProvider>
