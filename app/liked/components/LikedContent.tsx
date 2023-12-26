0ecd973cd6f28e8629ac93ab69a24639c5990c69
trainymonked <47102288+trainymonked@users.noreply.github.com>
2023-12-26 13:42:40 +0300

    Liked Songs

--- app/liked/components/LikedContent.tsx
+++ app/liked/components/LikedContent.tsx
@@ -1,14 +1,28 @@
 'use client'
 
-import LikeButton from '@/components/LikeButton'
-import MediaItem from '@/components/MediaItem'
-import { Song } from '@/types'
+import { useRouter } from 'next/navigation'
+import { useEffect } from 'react'
 
-interface SearchContentProps {
+import { Song } from '@/types'
+import { useUser } from '@/hooks/useUser'
+import MediaItem from '@/components/MediaItem'
+import LikeButton from '@/components/LikeButton'
+
+interface LikedContentProps {
     songs: Song[]
 }
 
-const SearchContent: React.FC<SearchContentProps> = ({ songs }) => {
+const LikedContent: React.FC<LikedContentProps> = ({ songs }) => {
+    const router = useRouter()
+
+    const { isLoading, user } = useUser()
+
+    useEffect(() => {
+        if (!isLoading && !user) {
+            router.replace('/')
+        }
+    }, [isLoading, user, router])
+
     if (songs.length === 0) {
         return (
             <div
@@ -21,13 +35,13 @@ const SearchContent: React.FC<SearchContentProps> = ({ songs }) => {
                     text-neutral-400
                 '
             >
-                No songs found
+                No liked songs
             </div>
         )
     }
 
     return (
-        <div className='flex flex-col gap-y-2 w-full px-6'>
+        <div className='flex flex-col gap-y-2 w-full p-6'>
             {songs.map((song) => (
                 <div key={song.id} className='flex items-center gap-x-4 w-full'>
                     <div className='flex-1'>
@@ -40,4 +54,4 @@ const SearchContent: React.FC<SearchContentProps> = ({ songs }) => {
     )
 }
 
-export default SearchContent
+export default LikedContent
