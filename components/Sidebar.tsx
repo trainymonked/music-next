bd662f06eb4995e28a41de73a47a9b227a6ceccd
trainymonked <47102288+trainymonked@users.noreply.github.com>
2023-12-26 12:53:12 +0300

    Songs Fetching and Search

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -4,15 +4,18 @@ import { usePathname } from 'next/navigation'
 import { useMemo } from 'react'
 import { HiHome } from 'react-icons/hi'
 import { BiSearch } from 'react-icons/bi'
+
 import Box from './Box'
 import SidebarItem from './SidebarItem'
 import Library from './Library'
+import { Song } from '@/types'
 
 interface SidebarProps {
     children: React.ReactNode
+    songs: Song[]
 }
 
-const Sidebar: React.FC<SidebarProps> = ({ children }) => {
+const Sidebar: React.FC<SidebarProps> = ({ children, songs }) => {
     const pathName = usePathname()
 
     const routes = useMemo(
@@ -41,7 +44,7 @@ const Sidebar: React.FC<SidebarProps> = ({ children }) => {
                         {routes.map((item) => (<SidebarItem key={item.label} {...item} />))}
                     </div>
                 </Box>
-                <Box className='overflow-y-auto h-full'><Library /></Box>
+                <Box className='overflow-y-auto h-full'><Library songs={songs} /></Box>
             </div>
             <main className='h-full flex-1 overflow-y-auto py-2'>{children}</main>
         </div>
