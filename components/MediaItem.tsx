6b56e370d1767003982898a91b1e689dd28d428d
trainymonked <47102288+trainymonked@users.noreply.github.com>
2023-12-29 17:01:51 +0300

    todone

--- components/MediaItem.tsx
+++ components/MediaItem.tsx
@@ -1,8 +1,10 @@
 'use client'
 
-import useLoadImage from '@/hooks/useLoadImage'
-import { Song } from '@/types'
 import Image from 'next/image'
+
+import { Song } from '@/types'
+import useLoadImage from '@/hooks/useLoadImage'
+import usePlayer from '@/hooks/usePlayer'
 
 interface MediaItemProps {
     data: Song
@@ -10,6 +12,7 @@ interface MediaItemProps {
 }
 
 const MediaItem: React.FC<MediaItemProps> = ({ data, onClick }) => {
+    const player = usePlayer()
     const imageUrl = useLoadImage(data)
 
     const handleClick = () => {
@@ -17,7 +20,7 @@ const MediaItem: React.FC<MediaItemProps> = ({ data, onClick }) => {
             return onClick(data.id)
         }
 
-        // turn on player
+        return player.setId(data.id)
     }
 
     return (
